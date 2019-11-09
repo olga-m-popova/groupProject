a0370a7b9e0eaef474a42c2ffbe7c317f088890c
katyasan <katyasan007@gmail.com>
2019-11-09 17:28:18 -0600

    added code

---
+++ src/katyasPractice/katya.java
@@ -0,0 +1,11 @@
+package katyasPractice;
+
+public class katya {
+    public static void main(String[] args) {
+
+        String school = "cybertek";
+        System.out.println(school);
+
+
+    }
+}
