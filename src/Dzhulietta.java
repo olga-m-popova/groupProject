34db1cd3b697b3cb6575d286633a37e977e27583
DzhulAleks <dzhulietta.ua@gmail.com>
2019-11-11 11:47:27 -0600

    add some notice

--- src/Dzhulietta.java
+++ src/Dzhulietta.java
@@ -2,6 +2,12 @@ public class Dzhulietta {
     public static void main(String[] args) {
         String newsOfTheDay = "Winter is here";
         System.out.println(newsOfTheDay);
+
+        int snow = 0;
+        if(snow > 5 ) {
+            System.out.println("Put on your snow boots");
+        }
+        System.out.println("Just be careful on the road");
     }
 
 }
